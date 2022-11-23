5768c0f84b970a6429bda8ef9613b638aa5f74b1
Jwilv <100983338+Jwilv@users.noreply.github.com>
2022-11-23 09:47:26 -0300

    Update new.tsx

--- pages/workers/new.tsx
+++ pages/workers/new.tsx
@@ -7,6 +7,6 @@ import FormPage from '../../components/form'
 export default function Form() {
 
     return (
-        <FormPage title='Hola mundo'/>
+        <FormPage title='AGREGAR WORKER'/>
     )
 }
